23dfdca7409399b10d44a4adecbf67b772e389d3
soufiane <soufiane.oucherrou@gmail.com>
2019-09-25 17:43:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,23 @@
 require "pry"
 
-def nyc_pigeon_organizer(cart)
+def pigeon_data(cart)
   final_hash={}
-  cart.each do |color_gender_lives, val_of_col_gend|
-    val_of_col_gend.each do |colors_item, arr|
-      arr.each do |person_name|
-        if cart[color_gender_lives][colors_item].include?(person_name)
-         final_hash["#{person_name}"]={
-           "#{color_gender_lives}": [] << "#{colors_item}"
-         }
-         #final_hash[person_name][color_gender_lives] << "#{colors_item}"
-        elsif !cart[color_gender_lives][colors_item].include?(person_name)
-          final_hash["#{person_name}"]={
-            "#{color_gender_lives}": ["#{colors_item}"]
+  cart.each do |color_gender_lives, values1|
+    values1.each do |colors_items, arr|
+      arr.each do |name|
+         if final_hash[name]
+          if final_hash[name][color_gender_lives]
+            final_hash[name][color_gender_lives] << colors_items
+          else
+            final_hash[name][color_gender_lives] = colors_items
+          end
+        else
+          final_hash[name]={
+            color_gender_lives => [colors_items]
           }
-          binding.pry
-        end
+         end
+      end
       end
     end
-  end
-  final_hash
+    final_hash
 end
