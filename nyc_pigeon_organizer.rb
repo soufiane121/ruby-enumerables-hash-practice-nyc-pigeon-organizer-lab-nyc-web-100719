9cef98a49c0b06e814552a13d94981938f6a5b3f
soufiane <soufiane.oucherrou@gmail.com>
2019-09-25 17:57:41 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require "pry"
+
+def nyc_pigeon_organizer(cart)
+  final_hash={}
+  cart.each do |color_gender_lives, values1|
+    values1.each do |colors_items, arr|
+      arr.each do |name|
+         if final_hash[name]
+          if final_hash[name][color_gender_lives]
+            final_hash[name][color_gender_lives] << colors_items.to_s
+          else
+            final_hash[name][color_gender_lives] = [colors_items.to_s]
+          end
+        else
+          final_hash[name]={
+            color_gender_lives => [colors_items.to_s]
+          }
+         end
+      end
+      end
+    end
+    final_hash
 end
